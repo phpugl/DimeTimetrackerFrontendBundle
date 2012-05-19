6bfb0e91e51178023a2e6e74bf07ebb8a91368f6
Danilo Kuehn <dk@nogo-software.de>
2012-05-19 16:45:59 +0200

    tweak track funtion with preventDefault

--- Resources/public/js/views/dashboard.js
+++ Resources/public/js/views/dashboard.js
@@ -6,42 +6,48 @@
   // Dashboard view
   App.provide('Views.Dashboard', Backbone.View.extend({
     el: $('body'),
-  	ui: {},
-  	events: {
-  		'submit #activity-track': 'trackActivity'
-  	},
-  	initialize: function() {
-        // Bind all to this, because you want to use
-        // "this" view in callback functions
-        _.bindAll(this);
-
-        this.activities = new App.Collection.Activities();
-        this.activityForm = new App.Views.Activity.Form({el: '#activity-form', collection: this.activities});
-        this.activityList = new App.Views.Activity.List({
-            collection: this.activities,
-            itemTagName: "section",
-            itemAttributes: {
-                "class": "activity"
-            },
-            form: this.activityForm
-        }).render();
-        this.activities.fetch();
-
-        // find ui elements
-        this.ui.activityInput = $('#activity-track-input');
+    ui: {},
+    events: {
+      'submit #activity-track': 'trackActivity'
     },
-  	render: function() {
+    initialize: function() {
+      // Bind all to this, because you want to use
+      // "this" view in callback functions
+      _.bindAll(this);
 
-  	},
-  	trackActivity: function(e) {
-        e.preventDefault();
+      // fetch activities
+      this.activities = new App.Collection.Activities();
+      this.activityForm = new App.Views.Activity.Form({
+        el: '#activity-form',
+        collection: this.activities
+      });
+      this.activityList = new App.Views.Activity.List({
+        collection: this.activities,
+        itemTagName: "section",
+        itemAttributes: {
+          "class": "activity"
+        },
+        form: this.activityForm
+      }).render();
+      this.activities.fetch();
 
-  		var data = this.ui.activityInput.val();
+      // find ui elements
+      this.ui.activityInput = $('#activity-track-input');
+    },
+    trackActivity: function(e) {
+      e.preventDefault();
 
-  		if (data && data !== "") {
-            this.activities.create({ parse: data }, {wait: true});
-        }
-  	}
+      var data = this.ui.activityInput.val(),
+          _this = this;
+      if (data && data !== "") {
+        this.activities.create({ parse: data }, {
+          wait: true,
+          success: function() {
+            _this.ui.activityInput.val('');
+          }
+        });
+      }
+    }
   }));
 
 })(jQuery, Dime);
