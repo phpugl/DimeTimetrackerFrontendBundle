9786ede66f8a2fc4c662938a661a8384f71646cb
Danilo Kuehn <dk@nogo-software.de>
2012-06-21 22:02:27 +0200

    now you can choose the date for tracking

--- Resources/public/js/app/activity/track.js
+++ Resources/public/js/app/activity/track.js
@@ -16,26 +16,35 @@
   App.provide('Views.Activity.Track', Backbone.View.extend({
     el: '#activity-track',
     events: {
+      'changeDate #activity-track-date': 'updateTitle',
       'submit': 'save'
     },
     initialize: function() {
       // Bind all to this, because you want to use
       // "this" view in callback functions
-      _.bindAll(this, 'save');
+      _.bindAll(this, 'save', 'updateTitle');
 
       this.activities = App.session('activities');
       if (!this.activities) {
         this.activities = App.session('activities', new App.Collection.Activities());
       }
     },
+    updateTitle: function(e) {
+        var icon = $('#activity-track-date');
+        icon.attr('title', icon.data('date'));
+
+        return this;
+    },
     save: function(e) {
       e.preventDefault();
-      var icon = $('i', '#activity-track').addClass('loading-14-white'),
-          input = $('#activity-track-input');
+      var input = $('#activity-track-input'),
+          icon = $('i', '#activity-track').addClass('loading-14-white');
 
-      var data = $('#activity-track-input').val();
+      var data = input.val(),
+          date = $('#activity-track-date').data('date') || moment().format('YYYY-MM-DD');
+
       if (data && data !== "") {
-        this.activities.create({parse: data}, {
+        this.activities.create({parse: data, date: date}, {
           wait: true,
           success: function() {
             input.val('');
