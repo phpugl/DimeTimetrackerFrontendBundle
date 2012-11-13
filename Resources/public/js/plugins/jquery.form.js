9fac4466cf372983c78f70d2565ccd7dc548b6dc
Danilo Kuehn <dk@nogo-software.de>
2012-11-13 10:34:19 +0100

    add parameter data(removeEmpty), will ignore undefined data from form

--- Resources/public/js/plugins/jquery.form.js
+++ Resources/public/js/plugins/jquery.form.js
@@ -4,7 +4,7 @@
  * jQuery - form
  *
  * handles form filling, clearing and data by input id
- * 
+ *
  * Usage:
  *
  * <form id="form-id" data-prefix="test-">
@@ -22,70 +22,91 @@
 
 (function ($) {
 
-  // 
-  $.fn.form = function() {
-    var $form = $(this),
-        prefix = $form.data('prefix') || $('form', $form).data('prefix') || '';
+    //
+    $.fn.form = function () {
+        var $form = $(this),
+            prefix = $form.data('prefix') || $('form', $form).data('prefix') || '';
 
-    return {
-      data: function() {
-        var data = {};
-        if ($form) {
-          $(':input', $form).each(function(idx, el) {
-            var $el = $(el);
-            if (el.id && el.id.search(prefix) != -1) {
-              data[el.id.replace(prefix, '')] = $el.val();
+        return {
+            data:function (removeEmpty) {
+                var data = {};
+                if ($form) {
+                    $(':input', $form).each(function (idx, el) {
+                        var $el = $(el);
+                        if (el.id && el.id.search(prefix) != -1) {
+                            var val = $el.val();
+                            if (removeEmpty) {
+                                if (val && val != '') {
+                                    if (el.type && el.type == 'checkbox' || el.type == 'radio') {
+                                        if ($el.attr('checked') == 'checked') {
+                                            data[el.id.replace(prefix, '')] = val;
+                                        }
+                                    } else {
+                                        data[el.id.replace(prefix, '')] = val;
+                                    }
+                                }
+                            } else {
+                                if (el.type && el.type == 'checkbox' || el.type == 'radio') {
+                                    if ($el.attr('checked') == 'checked') {
+                                        data[el.id.replace(prefix, '')] = val;
+                                    } else {
+                                        data[el.id.replace(prefix, '')] = undefined;
+                                    }
+                                } else {
+                                    data[el.id.replace(prefix, '')] = val;
+                                }
+                            }
+                        }
+                    });
+                }
+                return data;
+            },
+            get:function (name) {
+                var obj = undefined;
+                if ($form && name) {
+                    obj = $('#' + prefix + name, $form);
+                }
+                return obj;
+            },
+            errors:function (data) {
+                if ($form) {
+                    $('.control-group', $form).removeClass('error');
+
+                    if (data) {
+                        for (var name in data) if (data.hasOwnProperty(name)) {
+                            var input = $('#' + prefix + name, $form);
+                            if (input) {
+                                var group = input.parents('.control-group');
+                                group.addClass('error');
+                                $('span.help-inline', group).text(data[name]);
+                            }
+                        }
+                    }
+                }
+            },
+            fill:function (data) {
+                if ($form && data) {
+                    for (var name in data) if (data.hasOwnProperty(name)) {
+                        var input = $('#' + prefix + name, $form);
+                        if (input) {
+                            input.val(data[name]);
+                        }
+                    }
+                }
+            },
+            clear:function () {
+                $(':input', $form).each(function (idx, el) {
+                    var type = el.type;
+                    var tag = el.tagName.toLowerCase();
+                    if (type == 'text' || type == 'password' || tag == 'textarea')
+                        $(el).val("");
+                    else if (type == 'checkbox' || type == 'radio')
+                        el.checked = false;
+                    else if (tag == 'select')
+                        el.selectedIndex = -1;
+                });
             }
-          });
-        }
-        return data;
-      },
-      get: function(name) {
-        var obj = undefined;
-        if ($form && name) {
-          obj = $('#' + prefix + name, $form);
-        }
-        return obj;
-      },
-      errors: function(data) {
-        if ($form) {
-          $('.control-group', $form).removeClass('error');
-          
-          if (data) {
-            for (var name in data) if (data.hasOwnProperty(name)) {
-              var input = $('#' + prefix + name, $form);
-              if (input) {
-                var group = input.parents('.control-group');
-                group.addClass('error');
-                $('span.help-inline', group).text(data[name]);
-              }
-            }
-          }
-        }
-      },
-      fill: function(data) {
-        if ($form && data) {
-          for (var name in data) if (data.hasOwnProperty(name)) {
-            var input = $('#' + prefix + name, $form);
-            if (input) {
-              input.val(data[name]);
-            }
-          }
-        }
-      },
-      clear: function() {
-        $(':input', $form).each(function(idx, el) {
-          var type = el.type;
-          var tag = el.tagName.toLowerCase();
-          if (type == 'text' || type == 'password' || tag == 'textarea')
-            $(el).val("");
-          else if (type == 'checkbox' || type == 'radio')
-            el.checked = false;
-          else if (tag == 'select')
-            el.selectedIndex = -1;
-        });
-      }
+        };
     };
-  };
-  
+
 })(jQuery);
