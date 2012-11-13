41a6a0caede8e73574d4aa3deba3b9cd9423e98b
Danilo Kuehn <dk@nogo-software.de>
2012-11-13 10:33:02 +0100

    add format parameter

--- Resources/public/js/core/helper/format.js
+++ Resources/public/js/core/helper/format.js
@@ -12,10 +12,12 @@
      *
      * @param data, a duration in given unit, e.g. seconds
      * @param unit, default 'seconds' (see at moment,js)
+     * @param format, default 'HH:mm:ss'
      * @return string formatted as HH:mm:ss or d HH:mm:ss when more than 24 hours
      */
-    App.provide('Helper.Format.Duration', function (data, unit) {
+    App.provide('Helper.Format.Duration', function (data, unit, format) {
         unit = unit || 'seconds';
+        format = format || 'HH:mm:ss';
         var duration = moment.duration(data, unit);
         if (duration >= 86400000) {
             return Math.floor(duration.asDays()) + 'd '
