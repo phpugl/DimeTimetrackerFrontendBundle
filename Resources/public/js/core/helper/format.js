691e0cbb77894a5c24b5c7bcd45541126f5006bf
Danilo Kuehn <dk@nogo-software.de>
2013-02-01 13:30:24 +0100

    cleanup and reorder functions

--- Resources/public/js/core/helper/format.js
+++ Resources/public/js/core/helper/format.js
@@ -7,10 +7,16 @@
  */
 (function (App, $, moment) {
 
+    /**
+     * App.Helper.Format.Date
+     *
+     * @param date Date object, default: new Date
+     * @param format default: 'YYYY-MM-DD HH:mm:ss'
+     * @return {*}
+     */
     App.provide('Helper.Format.Date', function(date, format) {
         date = date || new Date;
         format = format || 'YYYY-MM-DD HH:mm:ss';
-
         return moment(date).format(format);
     });
 
@@ -45,26 +51,25 @@
     });
 
     /**
-     * App.Helper.Format.Truncate
+     * App.Helper.Format.EditableText
+     * Transform HTML Content from "contenteditable" to Text with \n
      *
-     * @param text, text to truncate
-     * @param length, default: 30
-     * @param endChars, default: '...'
-     * @return string truncate at first line break and after given length
+     * @param html html content
+     * @return string sanitized text
      */
-    App.provide('Helper.Format.Truncate', function (text, length, endChars) {
-        length = length || 30;
-        endChars = endChars || '...';
-
-        var result = /(.*)\n?/.exec(text);
-
-        if (result && result[1]) {
-            return (result[1].length > length) ? result[1].substr(0, 30) + endChars : result[1];
-        } else {
-            return text;
+    App.provide('Helper.Format.EditableText', function(html) {
+        var ce = $("<pre />").html(html);
+        if ($.browser.webkit) {
+            ce.find("div").replaceWith(function() { return "\n" + this.innerHTML; });
         }
+        if ($.browser.msie) {
+            ce.find("p").replaceWith(function() { return this.innerHTML + "<br>"; });
+        }
+        if ($.browser.mozilla || $.browser.opera || $.browser.msie) {
+            ce.find("br").replaceWith("\n");
+        }
+        return ce.text();
     });
-
 
     /**
      * App.Helper.Format.Nl2Br
@@ -75,7 +80,6 @@
     App.provide('Helper.Format.Nl2Br', function (str) {
         return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br />$2');
     });
-
 
     /**
      * App.Helper.Format.Slugify
@@ -97,25 +101,25 @@
     });
 
     /**
-     * App.Helper.Format.EditableText
-     * Transform HTML Content from "contenteditable" to Text with \n
+     * App.Helper.Format.Truncate
      *
-     * @param html html content
-     * @return string sanitized text
+     * @param text, text to truncate
+     * @param length, default: 30
+     * @param endChars, default: '...'
+     * @return string truncate at first line break and after given length
      */
-    App.provide('Helper.Format.EditableText', function(html) {
-        var ce = $("<pre />").html(html);
-        if ($.browser.webkit) {
-            ce.find("div").replaceWith(function() { return "\n" + this.innerHTML; });
+    App.provide('Helper.Format.Truncate', function (text, length, endChars) {
+        length = length || 30;
+        endChars = endChars || '...';
+
+        var result = /(.*)\n?/.exec(text);
+
+        if (result && result[1]) {
+            return (result[1].length > length) ? result[1].substr(0, 30) + endChars : result[1];
+        } else {
+            return text;
         }
-        if ($.browser.msie) {
-            ce.find("p").replaceWith(function() { return this.innerHTML + "<br>"; });
-        }
-        if ($.browser.mozilla || $.browser.opera || $.browser.msie) {
-            ce.find("br").replaceWith("\n");
-        }
-        return ce.text();
-    })
+    });
 
 })(Dime, jQuery, moment);
 
