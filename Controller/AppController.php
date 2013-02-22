9c1ee7d5486a853efa981cc2b9b55d11bd184495
Danilo Kuehn <dk@nogo-software.de>
2013-02-22 17:27:05 +0100

    add method for templates fetching, only GET allowed

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -4,6 +4,7 @@ namespace Dime\TimetrackerFrontendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -11,6 +12,7 @@ class AppController extends Controller
 {
     /**
      * @Route("/")
+     * @Method("GET")
      * @Template()
      */
     public function indexAction()
@@ -20,6 +22,7 @@ class AppController extends Controller
 
     /**
      * @Route("/template/{name}")
+     * @Method("GET")
      * @param  string   $name
      * @return Response A Response instance
      */
