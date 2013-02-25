c96783072b0289066a1408ab56c60781eddd41bb
Danilo Kuehn <dk@nogo-software.de>
2013-02-25 19:20:33 +0100

    rewrite tag widgets and add value function

--- Resources/public/js/core/views/widgets/tags.js
+++ Resources/public/js/core/views/widgets/tags.js
@@ -9,41 +9,45 @@
         options: {
             delimiter: ' '
         },
-        render: function(parent) {
-            if (parent) {
-                if (this.el === undefined) {
-                    this.setElement(parent.el);
-                }
-            }
-        },
         bind: function(data) {
             if (data && data.relation && data.relation.tags) {
                 this.$el.val(data.relation.tags.pluck('name').join(this.options.delimiter));
             } else {
                 this.$el.val('');
             }
+
+            return this;
         },
         name: function() {
-            return this.options.bind || this.el.name || this.el.id;
+            return this.options.bind || this.el.name;
         },
         serialize: function(data, withoutEmpty) {
             var name = this.name().split('-'),
-                value = this.$el.val();
+                value = this.value();
 
             if (withoutEmpty) {
-                if (value == undefined || _.isEmpty(value)) {
-                    return value;
+                if (value === undefined || _.isEmpty(value)) {
+                    return this;
                 }
-            }
-
-            if (value && value.length > 0) {
-                value = value.split(this.options.delimiter);
-            } else {
-                value = []
             }
 
             if (data) {
                 App.Helper.Object.Set(data, name, value);
+            }
+
+            return this;
+        },
+        value: function(value) {
+            if (value) {
+                this.$el.val(value);
+            }
+
+            value = $.trim(this.$el.val());
+
+            if (value && value.length > 0) {
+                value = _.compact(value.split(this.options.delimiter));
+            } else {
+                value = []
             }
 
             return value;
